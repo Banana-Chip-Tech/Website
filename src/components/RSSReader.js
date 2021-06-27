91d88efc9c67e41abdc99c9b3ca8af0599a813d4
Sam  Shenoi <samshenoi@Sams-MBP.attlocal.net>
2021-06-27 17:12:07 -0500

    Blanked out rss reader

--- src/components/RSSReader.js
+++ src/components/RSSReader.js
@@ -12,17 +12,7 @@ export default class RSSReader extends React.Component{
    render(){
        return(
            <div>
-            <Helmet>
-              <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>
-            </Helmet>
-            <h1 style={{textAlign:"center"}}>Blog</h1>
-            <div id="retainable-rss-embed" data-rss="https://bananachiptech.medium.com/feed"
-            data-maxcols="2"
-            data-layout="slider"
-            data-poststyle="inline"
-            data-readmore="Read the rest"
-            data-buttonclass="btn btn-primary"
-            ></div></div>
+            </div>
 
 
        )
