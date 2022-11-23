87be131c47e8895198f8442ad57487ee24fafcbf
Sam  Shenoi <samshenoi@Sams-MBP.hsd1.tx.comcast.net>
2022-11-23 16:30:22 -0600

    Updated jsdom

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "bananachiptech",
+    title: "Banana Chip Tech",
+    description:"Banana Chip Tech is a company dedicated to optimizing healthcare through computational means. We build apps, websites, and create coding courses to achieve this end!"
   },
+  flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: ["gatsby-plugin-image"],
 };
