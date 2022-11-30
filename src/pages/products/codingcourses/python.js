9d0e7a969722343d3e75879da7a869d165c661da
Sam  Shenoi <samshenoi@sams-mbp.ad.bcm.edu>
2022-11-30 10:16:24 -0600

    Finished conversion to ModuleCard, untested

--- src/pages/products/codingcourses/python.js
+++ src/pages/products/codingcourses/python.js
@@ -6,19 +6,6 @@ import ModuleCard from "../../../components/ModuleCard"
 
 // styles
 
-
-// Based off of https://stackoverflow.com/questions/60764233/how-to-optimize-this-if-else-block-to-compare-date-in-simple-react-app
-/*function compareDateWithoutTime(dateA, dateB) {
-  let date1 = new Date(dateA);
-  let date2 = new Date(dateB);
-  date1.setHours(0, 0, 0, 0);
-  date2.setHours(0, 0, 0, 0);
-  return date2.getTime() - date1.getTime();
-}*/
-
-
-
-
 // markup
 const PythonPage = () => {
   return (
@@ -35,181 +22,55 @@ const PythonPage = () => {
         </p>
       </div>
       <Accordion style={{margin:"1%"}} defaultActiveKey="0">
-        {/*<Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">Lesson 1: Introduction to python3</Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>
-              <h3 style={{textAlign:"center"}}>Lesson 1 Resources</h3>
-              <Container>
-                <Row style={{paddingBottom:"2%"}}>
-                  <Col ><iframe width="560" height="315" src="https://www.youtube.com/embed/NaLtBvxNIPE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  </Col>
-                  <Col style={{margin:"1.5%"}}>
-                  <p>This lesson introduces the following python concepts!</p>
-                  <ul>
-                    <li>What is programming?</li>
-                    <li>What is a programming language?</li>
-                    <li>What is python?</li>
-                    <li>How to install python</li>
-                    <li>Text Editors and IDEs for coding in python</li>
-                  </ul>
-
-                  <p>Refer to the following resources for written tutorials!</p>
-                  <ListGroup>
-                    <ListGroup.Item >
-                    <a href="https://bananachiptech.medium.com/introduction-to-programming-dad44b524e05">Introduction to Programming</a>
-                    </ListGroup.Item>
-
-                  </ListGroup>
-                  </Col>
-                </Row>
-                </Container>
-
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-             Lesson 2: Input and Output
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>
-            <h3 style={{textAlign:"center"}}>Lesson 2 Resources</h3>
-              <Container>
-                <Row style={{paddingBottom:"2%"}}>
-                  <Col ><iframe style={{margin:"1.5%"}} width="560" height="315" src="https://www.youtube.com/embed/QZgGpCXGH54" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                  </Col>
-                  <Col style={{margin:"1.5%"}}>
-                  <p>This lesson introduces the following python concepts!</p>
-                  <ul>
-                    <li>Input</li>
-                    <li>Output</li>
-                    <li>Comments</li>
-                  </ul>
-
-                  <p>Refer to the following resources for written tutorials!</p>
-                  <ListGroup>
-                    <ListGroup.Item >
-                       <a href="https://bananachiptech.medium.com/dealing-with-input-and-output-in-python-e838f56fac90">Input and Output </a>
-                    </ListGroup.Item>
-
-                  </ListGroup>
-                  </Col>
-                </Row>
-                </Container>
-
-        <h3 style={{textAlign:"center"}}>Homework 1 </h3>
-        <Container>
-          <Row>
-            <Col>
-            <ListGroup>
-                    <ListGroup.Item >
-                    Write a program that prints hello world to the screen
-                    </ListGroup.Item>
-                    <ListGroup.Item >
-                    Write a program that asks the user to input their name, age, and favorite food.
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                    Write a program that asks the user for their name and prints it back out to the user.
-                    </ListGroup.Item>
-                    <ListGroup.Item>Read information from the <a href="/HW1input.txt" download> input.txt</a> file. Display this information back out to the user.</ListGroup.Item>
-            </ListGroup>
-            <ListGroup>
-              <br></br>
-             <p>Refer to the following for written homework solutions.</p>
-            <ListGroup.Item >
-                       <a href="https://bananachiptech.medium.com/input-and-output-practice-questions-6b22e961e03b">Input and Output Homework Solutions </a>
-                    </ListGroup.Item>
-            </ListGroup>
-
-            </Col>
-            <Col>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/rRp4is2MExo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </Col>
-          </Row>
-        </Container>
-
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="2">
-             Lesson 3: Variables
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="2">
-            <Card.Body>
-              <h3 style={{textAlign:"center"}}>Lesson 3 Resources</h3>
-              <Container>
-                <Row style={{paddingBottom:"2%"}}>
-                  <Col ><iframe style={{margin:"1.5%"}} width="560" height="315" src="https://www.youtube.com/embed/CsybBZ-TnSI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></Col>
-                  <Col style={{margin:"1.5%"}}>
-                  <p>This lesson introduces the following different types of variables in python!</p>
-                  <Table bordered striped>
-                    <thead><th>Basic Variable Types</th><th>Data Structure Variable Types</th></thead>
-                    <tr><td>int</td><td>string</td></tr>
-                    <tr><td>float</td><td>list</td></tr>
-                    <tr><td>boolean</td><td>tuple</td></tr>
-                    <tr><td>char</td><td>dictionary</td></tr>
-                  </Table>
-                  <p>Refer to the following resources for written tutorials!</p>
-                  <ListGroup>
-                    <ListGroup.Item >
-                       <a href="https://bananachiptech.medium.com/basic-variable-types-in-python-4a121326cfe4">Basic Variable Types</a>
-
-                    </ListGroup.Item>
-                    <ListGroup.Item >
-                    <a href="https://medium.com/codex/data-structure-variable-types-666c837bec0f">Data Structure Variable Types</a>
-                    </ListGroup.Item>
-                  </ListGroup>
-                  </Col>
-                </Row>
-                </Container>
-
-        <h3 style={{textAlign:"center"}}>Homework 2 </h3>
-        <Container>
-          <Row>
-            <Col>
-            <ListGroup>
-                    <ListGroup.Item >
-                      Ask the user for a number. Multiply the number by 10 and print it back out to the user
-                    </ListGroup.Item>
-                    <ListGroup.Item >
-                    Ask the user for two numbers. Divide the first number by the second number and print out the result.
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                    Ask the user for their name, age, and favorite food. Print out 1 message to the user with all of this information.
-                    </ListGroup.Item>
-                    <ListGroup.Item>Ask the user for their year of birth. Calculate how old they are based on todays year. Return this result back out to the user</ListGroup.Item>
-                    <ListGroup.Item>Ask the user for three numbers and make a list out of these numbers. Print out the resulting list.</ListGroup.Item>
-                    <ListGroup.Item>Access the first and last element in a list and print it out to the screen. Make sure to test your code on lists of various sizes! Hint: <code>len(<em>your list variable</em>)</code> returns the number of elements in the list</ListGroup.Item>
-                    <ListGroup.Item>Create a 10 element long list. Ask the user for a number between 1 and 10 and another number. Insert the second number into your created list at the position of the first number.
-          <br></br>Example:
-          <br></br>
-          <code>
-          User Input: 3, 50<br></br>
-          samplestartinglist = [1,2,3,4,5,8,6,9,10]<br></br>
-
-          Program Output: [1,2,3,50,5,8,6,9,10]
-          </code></ListGroup.Item>
-                  </ListGroup>
-
-            </Col>
-            <Col>
-            <iframe style={{margin:"6%"}} width="560" height="315" src="https://www.youtube.com/embed/w3wk6n_2eJY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </Col>
-          </Row>
-        </Container>
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-
-          */}
+          <ModuleCard
+          lessonNumber={1}
+          lessonName="Introduction to python3"
+          concepts={["What is programming?","What is a programming language?","What is python?","How to install python","Text Editors and IDEs for coding in python",]}
+          lessonLink="https://www.youtube.com/embed/NaLtBvxNIPE"
+          articles={[{name:"Introduction to Programming","link":"https://bananachiptech.medium.com/introduction-to-programming-dad44b524e05"}]}
+          lessonReleaseDate={new Date(2021,6,11)}
+        ></ModuleCard>
+          <ModuleCard
+          lessonNumber={2}
+          lessonName="Input and Output"
+          concepts={["Input","Output","Comments"]}
+          lessonLink="https://www.youtube.com/embed/QZgGpCXGH54"
+          hwvideo="https://www.youtube.com/embed/rRp4is2MExo"
+          hw={[ <p>Write a program that prints hello world to the screen</p>,
+            <p>Write a program that asks the user to input their name, age, and favorite food.</p>,
+            <p>Write a program that asks the user for their name and prints it back out to the user.</p>,
+            <p>Read information from the <a href="/HW1input.txt" download> input.txt</a> file. Display this information back out to the user.</p>
+          ]}
+          articles={[{name:"Input and Output","link":"https://bananachiptech.medium.com/dealing-with-input-and-output-in-python-e838f56fac90"},{"link":'https://bananachiptech.medium.com/input-and-output-practice-questions-6b22e961e03b',name:"Input/Output HW Solutions"}]}
+          lessonReleaseDate={new Date(2021,6,11)}
+          hwReleaseDate={new Date(2021,6,14)}
+        ></ModuleCard>
+          <ModuleCard
+          lessonNumber={3}
+          lessonName="Conditionals"
+          concepts={["Basic Variable Types (int,float,boolean,char)","Data Structure Variable Types (string,list,tuple,dictionary)"]}
+          lessonLink="https://www.youtube.com/embed/CsybBZ-TnSI"
+          hwvideo="https://www.youtube.com/embed/w3wk6n_2eJY"
+          hw={[<p>Ask the user for a number. Multiply the number by 10 and print it back out to the user</p>,
+                <p>Ask the user for two numbers. Divide the first number by the second number and print out the result.</p>,
+               <p>Ask the user for their name, age, and favorite food. Print out 1 message to the user with all of this information.</p>,
+                <p>Ask the user for their year of birth. Calculate how old they are based on todays year. Return this result back out to the user</p>,
+                <p>Ask the user for three numbers and make a list out of these numbers. Print out the resulting list.</p>,
+                <p>Access the first and last element in a list and print it out to the screen. Make sure to test your code on lists of various sizes! Hint: <code>len(<em>your list variable</em>)</code> returns the number of elements in the list</p>,
+                <div><p>Create a 10 element long list. Ask the user for a number between 1 and 10 and another number. Insert the second number into your created list at the position of the first number.</p>
+                    <br></br>Example:
+                    <br></br>
+                    <code>
+                    User Input: 3, 50<br></br>
+                    samplestartinglist = [1,2,3,4,5,8,6,9,10]<br></br>
+                    Program Output: [1,2,3,50,5,8,6,9,10]
+                    </code>
+                </div>
+          ]}
+          articles={[{name:"Basic Variable Types","link":"https://bananachiptech.medium.com/basic-variable-types-in-python-4a121326cfe4"},{"link":'https://medium.com/codex/data-structure-variable-types-666c837bec0f',name:"Data Structure Variable Types"}]}
+          lessonReleaseDate={new Date(2021,6,11)}
+          hwReleaseDate={new Date(2021,6,14)}
+        ></ModuleCard>
         <ModuleCard
           lessonNumber={4}
           lessonName="Conditionals"
